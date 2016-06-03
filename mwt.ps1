71e2bd5f4e764c7d951ee29c71880d39a6878000
Semicolon <michaeltri.gg@gmail.com>
2016-06-03 14:56:40 -0500

    Initial Template
    My New Script Template

---
+++ mwt.ps1
@@ -0,0 +1,23 @@
+﻿<#	
+ .Synopsis
+
+ .DESCRIPTION
+
+ .EXAMPLE
+
+ .EXAMPLE
+
+ .NOTES
+    Created on:  mm/dd/yyyy
+    Created by:  Semicolon (Michael Trigg)
+    Script:      mwt.ps1
+
+
+    LinkedIn:    https://www.linkedin.com/in/mtrigg
+    GitHub:      https://github.com/mwtrigg
+    Spiceworks:  https://community.spiceworks.com/people/michaeltrigg
+    Twitter:     https://twitter.com/mwtrigg
+    
+ .LINK
+		
+#>
